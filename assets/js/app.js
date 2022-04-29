09e8798c2071bbd570190bec6f9deb1c81c898d2
MirkOrlando <mirko@orlando-m.it>
2022-04-29 15:16:11 +0200

    printed all emails

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
 const app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+        my_data: null,
+        emails: null,
+    },
     methods: {},
     mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            console.log(this);
-            console.log(response);
-        })
+        this.my_data = []
+        this.emails = []
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                //console.log(this);
+                //console.log(response);
+                //console.log(response.data)
+                this.my_data.push(response.data)
+                    //console.log(this.my_data);
+                    /*                 if (this.my_data.length = 10) {
+                                        this.emails = this.my_data
+                                    }
+                     */
+            })
+        }
     }
 })
