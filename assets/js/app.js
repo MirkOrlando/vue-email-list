dfbbe1d44c07c7b50773392907b6e54104f07dbb
MirkOrlando <mirko@orlando-m.it>
2022-04-29 14:40:40 +0200

    created static markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {},
     methods: {},
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+            console.log(this);
+            console.log(response);
+        })
+    }
 })
